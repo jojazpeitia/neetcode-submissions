496952588b9a2874cf8123cafa47a7b6c9610057
Jorge Azpeitia <83856029+jojazpeitia@users.noreply.github.com>
2026-09-10 15:55:08 -0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+
+        # create a list that captures the count of characters in a word
+        # use that list as a key 
+        # attach the word to be the value
+
+        # return the list of all values
+
+        my_dict = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            my_dict[tuple(count)].append(s)
+
+        return list(my_dict.values())
+
+            
+
+        
